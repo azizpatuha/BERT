0276d25d22eb8d1ba9bbfc169f06917242ce783e
Malik Abdul Aziz <58964618+azizpatuha@users.noreply.github.com>
2024-12-13 08:49:43 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+from transformers import BertTokenizer, TFBertForSequenceClassification
+import tensorflow as tf 
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('indobenchmark/indobert-base-p2')
+    model = TFBertForSequenceClassification.from_pretrained("azizpatuha/BERT")
+    return tokenizer, model
+
+tokenizer, model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
+
+d = {
+    1: 'negatif',
+    0: 'positif'
+}
+
+if user_input and button:
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512, return_tensors='tf')
+    
+    output = model(**test_sample)
+    st.write("Logits: ", output.logits)
+    
+    y_pred = np.argmax(output.logits.numpy(), axis=1)  # .numpy() untuk mengambil nilai array
+    st.write("Prediction: ", d[y_pred[0]])
